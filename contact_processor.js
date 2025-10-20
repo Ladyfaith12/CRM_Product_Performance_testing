594f80b26b648644b4fab854b73c9342c45ed759
Ladyfaith12 <faith.mensah@purplewave.com>
2025-10-20 16:00:15 +0000

    finishing up with the create contact resources

--- contact_processor.js
+++ contact_processor.js
@@ -2,10 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 function getContactData(context, events, done) {
-  const dataPath = path.join(__dirname, 'contact-data.json');
-  const contactData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
-  context.vars.contactData = contactData;
-  return done();
+  // Use __dirname to get the path relative to THIS file's location
+  const dataPath = path.join(__dirname, 'contactPayload.json');
+  
+  try {
+    const contactData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+    context.vars.contactData = contactData;
+    return done();
+  } catch (error) {
+    console.error('Error reading contact data:', error.message);
+    return done(error);
+  }
 }
 
 module.exports = {
